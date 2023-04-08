f6f02a1061f8cedeff04108791f812e5cfa5bc5b
Felipe Carvalho <fcarvalhoosilva@gmail.com>
2023-04-08 16:03:53 -0300

    first commit

---
+++ script.js
@@ -0,0 +1,34 @@
+// selecionando o elemento HTML com a classe desejada
+const dlink1 = document.querySelector('.dlink--1');
+
+// adicionando um ouvinte de eventos de clique ao elemento selecionado
+dlink1.addEventListener('click', function() {
+  // modificando a frase do elemento selecionado
+  dlink1.innerHTML = 'Moscow Mule<br>50ml de Vodka<br>30ml de xarope de gengibre<br>Soda<br>Espulma de gengibre';
+});
+
+// selecionando o elemento HTML com a classe desejada
+const dlink2 = document.querySelector('.dlink--2');
+
+// adicionando um ouvinte de eventos de clique ao elemento selecionado
+dlink2.addEventListener('click', function() {
+  // modificando a frase do elemento selecionado
+  dlink2.innerHTML = 'Fruits coquetel<br>50ml de gin<br>30ml de aperol<br>30ml de espulmante<br>Agua c/ gas para completar<br>Frutas<br>gelo';
+});
+
+// selecionando o elemento HTML com a classe desejada
+const dlink3 = document.querySelector('.dlink--3');
+
+// adicionando um ouvinte de eventos de clique ao elemento selecionado
+dlink3.addEventListener('click', function() {
+  // modificando a frase do elemento selecionadode
+  dlink3.innerHTML = 'Clericot<br>50ml licor de laranja<br>25ml de xarope de açucar<br>100ml de espulmante<br>Laranja e kiwi<br>gelo';
+});
+// selecionando o elemento HTML com a classe desejada
+const dlink4 = document.querySelector('.dlink--4');
+
+// adicionando um ouvinte de eventos de clique ao elemento selecionado
+dlink4.addEventListener('click', function() {
+  // modificando a frase do elemento selecionadode
+  dlink4.innerHTML = 'Limoncelo spritz<br>50ml de vodka<br>30ml de limoncello<br>15ml de limão<br>100ml de espulmante<br>completar com agua de soda';
+});
